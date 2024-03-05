ae711015af2d5c29df7a4856cf55e242786698b8
emmaderbe <emma-derbe@mail.ru>
2024-03-05 17:15:00 +0100

    add photos, applcon

--- PetApp/Constants/StringConstants.swift
+++ PetApp/Constants/StringConstants.swift
@@ -18,14 +18,14 @@ struct StringConstants {
     static let idDesCardData = "Designer"
     
     static let titleDevCardData = "Эмма"
-    static let titleDocCardData = "Левченко Полина Викторовна"
+    static let titleDocCardData = "Полина"
     static let titleDesCardData = "Анастасия"
     
     static let descriptionDevCardData = "Создание PetDiet стало для меня возможностью объединить 2 мои страсти - любовь к животным и технологиям. Скоро проект станет ещё универсальнее, благодаря поддержке других видов питомцев.\n\nЯ постоянно открыта для новых вызовов и интересных проектов. Если вам нужен разработчик с креативным мышлением и страстью к своей работе - свяжитесь со мной!"
-    static let descriptionDocCardData = "Я являюсь сертифицированным и практикующим ветеринарным врачом-диетологом. \n\nВ данном проекте мной была произведена работа по выборке наиболее опасных, а также допустимых продуктов к употреблению вашими питомцами."
+    static let descriptionDocCardData = "Я, Левченко Полина Викторовна,  сертифицированный и практикующий ветеринарный врач-диетолог. \n\nПомогала составлять и проверять список опасных и допустимых продуктов для собак."
     static let descriptionDesCardData = "Эмма пришла ко мне с идеей, и я приступила к созданию дизайна. У нас впереди много работы — в  планах добавить питание для кроликов и морских свинок. \n\nЯ открыта к сотрудничеству и буду рада поработать над новым продуктом :)"
     
     static let profileURLDev = "https://github.com/emmaderbe"
     static let profileURLDoc = "https://www.vet-diet.ru/"
-    static let profileURLDes = "https://www.vet-diet.ru/"
+    static let profileURLDes = "https://www.behance.net/stasime"
 }
