5b8c8fbd055b97f0171a0d972528f01529e1ef3f
Emma Derbe <emma-derbe@mail.ru>
2024-08-15 20:49:18 +0200

    edit ti new version, clear core data

--- PetApp/Application/AppDelegate.swift
+++ PetApp/Application/AppDelegate.swift
@@ -3,41 +3,63 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    lazy var persistentContainer: NSPersistentContainer = {
-            let container = NSPersistentContainer(name: "CoreDataModel")
-            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-                if let error = error {
-                    fatalError("Unresolved error \(error), \(error.localizedDescription)")
-                }
-            })
-            return container
-        }()
-
-        func saveContext () {
-            let context = persistentContainer.viewContext
-            if context.hasChanges {
-                do {
-                    try context.save()
-                } catch {
-                    let nserror = error as NSError
-                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-                }
-            }
-        }
-    
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         ValueTransformer.setValueTransformer(StringArrayTransformer(), forName: NSValueTransformerName("StringArrayTransformer"))
+        checkAndUpdateDataVersion()
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreDataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                fatalError("Unresolved error \(error), \(error.localizedDescription)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    func clearCoreData() {
+        guard let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext else { return }
+        
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "FavouriteProduct")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        
+        do {
+            try context.execute(deleteRequest)
+            try context.save()
+        } catch {
+            print("Error deleting data from Core Data: \(error)")
+        }
+    }
+    
+    func checkAndUpdateDataVersion() {
+        let currentDataVersion = 2
+        let savedVersion = UserDefaults.standard.integer(forKey: "dataVersion")
+        if savedVersion < currentDataVersion {
+            clearCoreData()
+            UserDefaults.standard.set(currentDataVersion, forKey: "dataVersion")
+        }
     }
 
 }
